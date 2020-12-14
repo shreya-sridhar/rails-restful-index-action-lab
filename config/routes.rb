225e8503f84204f4d0c735f3e581f067c0cea1ac
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-12-14 10:29:30 -0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/students', to: 'students#index'
 end
+
+
+
+# In your view, iterate over the list of students returned from the index action in the controller and display the appropriate information.
